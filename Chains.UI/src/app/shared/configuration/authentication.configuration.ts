0298379b2a476587061ee05a4e8c99b4a1b57946
Andy Brown <adrb1984@gmail.com>
2018-01-17 12:10:43 +0000

    - Change credentials to new app

--- Chains.UI/src/app/shared/configuration/authentication.configuration.ts
+++ Chains.UI/src/app/shared/configuration/authentication.configuration.ts
@@ -2,7 +2,7 @@ export const authenticationConfiguration = {
     production: false,
     auth0: {
       domain: 'chains.eu.auth0.com',
-      clientId: 'q9h6uxP3YMBMMNjI6DFjCX6eNODtJxjl',
+      clientId: 'yCU5mxpbv7rY5WUsfE5b1UV5fjIxh4oP',
       callbackURL: 'http://localhost:3001/callback'
     }
   };
