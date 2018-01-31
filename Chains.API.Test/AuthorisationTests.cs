3e2d1fb49380c8c24bd700a330ba7c88d9ecd5a8
Andy Brown <adrb1984@gmail.com>
2018-01-31 15:27:42 +0000

    - add testing to ensure that the auth0 endpoint is working - add rest sharp package to handle api calls to auth0

---
+++ Chains.API.Test/AuthorisationTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Chains.API.Repositories;
+using NUnit.Framework;
+
+namespace Chains.API.Test
+{
+    [TestFixture]
+    public class AuthorisationTests
+    {
+        [Test]
+        public void When_an_authorisation_token_is_requested__Then_the_token_returned_is_feasible()
+        {
+            AuthorisationRepository authRepo = new AuthorisationRepository();
+            var authorisationHeader = authRepo.GetAccessTokenHeaderForApi();
+            Assert.That(authorisationHeader.StartsWith("Bearer "));
+            Assert.That(authorisationHeader.Length > 50);
+        }
+    }
+}
